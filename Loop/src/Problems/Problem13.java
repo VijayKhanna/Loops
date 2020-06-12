39efa57042d7039d48bf72ddc709724eae58be2d
mylaptop <mylaptop@192.168.0.9>
2020-06-12 09:34:51 +0530

    MilestoneProblems

---
+++ Loop/src/Problems/Problem13.java
@@ -0,0 +1,24 @@
+package Problems;
+import java.util.Scanner;
+public class Problem13 {
+public static void main(String args[])
+{
+	Scanner sc= new Scanner(System.in);
+	int s=10,e=99;
+    while (s<e)
+    {
+    boolean flag = false;
+    for(int i=2;i<=s/2;i++) 
+    {
+            if(s%i == 0) 
+            {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag && s!= 0 && s != 1)
+        System.out.print(s+ " ");
+        s++;
+    }
+}
+}
