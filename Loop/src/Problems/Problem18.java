3ce3aa108f1ccc32096cb1c1b2c73f47e0c02eae
mylaptop <mylaptop@192.168.0.9>
2020-06-13 08:04:25 +0530

    MileStoneProblems

---
+++ Loop/src/Problems/Problem18.java
@@ -0,0 +1,22 @@
+package Problems;
+
+import java.util.Scanner;
+
+public class Problem18 {
+	public static void main(String args[]) {
+		int r, sum = 0, n;
+		Scanner sc = new Scanner(System.in);
+		int num;
+		num = sc.nextInt();
+		n = num;
+		while (n > 0) {
+			r = n % 10;
+			sum = (sum * 10) + r;
+			n = n / 10;
+		}
+		if (num == sum)
+			System.out.println("palindrome number ");
+		else
+			System.out.println("not palindrome");
+	}
+}
